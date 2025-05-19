39660034471ff84b8eac80ea5be7c5a485f95321
Scepty-SSB <168497715+Scepty-SSB@users.noreply.github.com>
2025-05-19 09:46:59 -0700

    add struct array and path check to main.c

--- Kitten-Stall/main.c
+++ Kitten-Stall/main.c
@@ -3,59 +3,83 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct manager {
+	const char* name;
+	const char* search;
+	const char* install;
+	_Bool enabled;
+};
+
+struct manager managerList[3] = {
+	{"apt", "apt-cache search", "apt install", true},
+	{"yay", "yay -s", "yay -S", false},
+	{"flatpak", "flatpak search", "flatpak install", true},
+};
 
 void print_usage(void) {
-  fprintf(stderr, "Pass -a to add.\n");
+	fprintf(stderr, "Pass -a to add.\n");
 }
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    print_usage();
-    return 1;
-  }
-  if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-    print_usage();
-    return 0;
-  }
-  
-  //creating the variable that points to the config file. Done this way because 
-  //every other methood I tried didn't work. I know it's probably bad, but I spent
-  //way too long figuring this out to care. If it ain't broke...
-  char const* const home = getenv("HOME");
-  char const config[] = "/.config/kittenstall/mgrs.conf";
-  
-  // build concatenated full path to config.
-  char* const mgrs_config = malloc(strlen(home) + strlen(config) + 1);
-  strcpy(mgrs_config, home);
-  strcat(mgrs_config, config);
-  
-  if (strcmp(argv[1], "--add") == 0 || strcmp(argv[1], "-a") == 0) {
-    //Opens in append mode if mgrs.conf exists, creates mrgs.conf and opens in
-    //write mode if not. Fixes blank lines at the beginning of the mgrs.conf
-    
-    FILE* const fptr = fopen(mgrs_config, access(mgrs_config, R_OK | W_OK) == 0 ? "a" : "w");
-    
-    if (fptr == NULL) {
-      fprintf(stderr, "opening %s failed.\n", mgrs_config);
-      return 2;
+	if (argc < 2) {
+		print_usage();
+		return 1;
+	}
+	if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+		print_usage();
+		return 0;
+	}
+
+	//creating the variable that points to the config file. Done this way because
+	//every other methood I tried didn't work. I know it's probably bad, but I spent
+	//way too long figuring this out to care. If it ain't broke...
+	char const* const home = getenv("HOME");
+	char const config[] = "/.config/kittenstall/mgrs.conf";
+
+	// build concatenated full path to config.
+	char* const mgrs_config = malloc(strlen(home) + strlen(config) + 1);
+	strcpy(mgrs_config, home);
+	strcat(mgrs_config, config);
+
+	if (strcmp(argv[1], "--add") == 0 || strcmp(argv[1], "-a") == 0) {
+		//Opens in append mode if mgrs.conf exists, creates mrgs.conf and opens in
+		//write mode if not. Fixes blank lines at the beginning of the mgrs.conf
+
+		FILE* const fptr = fopen(mgrs_config, access(mgrs_config, R_OK | W_OK) == 0 ? "a" : "w");
+
+		if (fptr == NULL) {
+			fprintf(stderr, "opening %s failed.\n", mgrs_config);
+			return 2;
+		}
+		for (int i = 2; i < argc; i++) {
+			fprintf(fptr, "%s\n", argv[i]);
+		}
+		fclose(fptr);
+	}
+	else {
+		printf("Enter the number of matches from each repository to list (0 to cancel)\n");
+		int matches;
+		scanf("%d", &matches);
+		if (matches == 0) {
+			printf("Program terminated by user");
+			return 0;
+		}
+		else {
+			printf("%d", matches);
+		}
+    //pointer arithmetic to get size of managerList[] because the size of elements 
+    //varies depending on the manager's name and specific commands
+    //I'll improve this at some point
+		for (int i = 0; i < *(&managerList + 1) - managerList; i++) {
+			printf ("%s, %s, %s, %b\n", managerList[i].name, managerList[i].search,
+			        managerList[i].install, managerList[i].enabled);
     }
-    for (int i = 2; i < argc; i++) {
-      fprintf(fptr, "%s\n", argv[i]);
-    }
-    fclose(fptr);
-  } 
-  else {
-      printf("Enter the number of matches from each repository to list (0 to cancel)\n");
-      int matches;
-      scanf("%d", &matches);
-      if (matches == 0) {
-          printf("Program terminated by user");
-          return 0;
-      }
-      else{
-         printf("%d", matches); 
-      }  
-  }
-  return 0;
-  }
-  
+    //wasn't sure this would work tbh
+    //I plan to check for if the package manager exists in $PATH rather than
+    //needing mgrs.conf at all
+		char * path = getenv("PATH");
+		printf("%s\n", path);
+	}
+	return 0;
+}
+
